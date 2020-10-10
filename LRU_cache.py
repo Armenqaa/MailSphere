f7c632ae1caf6728c1eb7222f365d2eac8cf0c76
Armen Gevorkyan <42323armen@gmail.om>
2020-10-11 00:51:42 +0300

    LRU_cache w/ get priority

--- LRU_cache.py
+++ LRU_cache.py
@@ -2,7 +2,7 @@ def min_priority(cache_dict):
     minimum_priority = 0
     minimum_priority_key = 0
     for key in cache_dict:
-        if cache_dict[key][1] <= minimum_priority && cache_dict[key][0] != '':
+        if cache_dict[key][1] <= minimum_priority and cache_dict[key][1] != -1:
             minimum_priority = cache_dict[key][1]
             minimum_priority_key = key
     return minimum_priority_key
@@ -16,6 +16,7 @@ class ICache:
 
     def get(self, key: str) -> str:
         try:
+            self.cache_dict[key][1] += 1
             return self.cache_dict[key][0]
         except KeyError:
             print('Такого ключа не существует')
@@ -23,14 +24,27 @@ class ICache:
 
     def set(self, key: str, value: str) -> None:
         if self.current_cache_size == self.max_cache_size:
-            print('Кэш заполнен')
-        else:
-            self.current_cache_size += 1
-            self.cache_dict[key] = [value, 0]
+            self.delete(min_priority(self.cache_dict))
+        self.current_cache_size += 1
+        self.cache_dict[key] = [value, 0]
 
     def delete(self, key: str) -> None:
         self.current_cache_size -= 1
         try:
             self.cache_dict[key][0] = ''
+            self.cache_dict[key][1] = -1
         except KeyError:
             print('Такого ключа не существует')
+
+
+if __name__ == '__main__':
+    cache = ICache(3)
+    cache.set('Jesse', 'Pinkman')
+    cache.set('Patrick', 'James')
+    cache.set('Walter', 'White')
+    cache.get('Walter')
+    cache.set('Armen', 'Gevorkyan')
+    print(cache.get('Jesse'))
+    print(cache.get('Walter'))
+    print(cache.get('Patrick'))
+    print(cache.get('Armen'))
